4334dd720924e001899883b2e290849dc6d9f68d
Guillaume Pellerin <yomguy@parisson.com>
2010-12-06 08:37:54 +0000

    tags 0.4.3

--- deefuzzer.py
+++ deefuzzer.py
@@ -45,7 +45,7 @@ import platform
 from threading import Thread
 from tools import *
 
-version = '0.4.2'
+version = '0.4.3'
 year = datetime.datetime.now().strftime("%Y")
 platform_system = platform.system()
 
