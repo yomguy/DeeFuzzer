c4f7fb4a20b42b91ac116b2034f19e747f187379
Guillaume Pellerin <yomguy@parisson.com>
2011-05-31 13:14:40 +0000

    reorganize the python package, get a real setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''The setup and build script for the python-twitter library.'''
+
+import os
+
+__author__ = 'yomguy@parisson.com'
+__version__ = '0.5.2'
+
+
+# The base package metadata to be used by both distutils and setuptools
+METADATA = dict(
+  name = "DeeFuzzer",
+  version = __version__,
+  py_modules = ['deefuzzer'],
+  description='an easy and instant media streaming tool',
+  author='Guillaume Pellerin', 
+  author_email='yomguy@parisson.com',
+  license='Gnu Public License V2',
+  url='http://svn.parisson.org/deefuzzer',
+  keywords='audio wideo streaming broadcast shout',
+  install_requires = ['setuptools','tinyurl'],
+  packages=['deefuzzer'], 
+  include_package_data = True,
+  scripts=['scripts/deefuzzer'], 
+  classifiers = ['Programming Language :: Python', 'Topic :: Internet :: WWW/HTTP :: Dynamic Content', 'Topic :: Multimedia :: Sound/Audio', 'Topic :: Multimedia :: Sound/Audio :: Players',],
+)
+
+
+def Main():
+  # Use setuptools if available, otherwise fallback and use distutils
+  try:
+    import setuptools
+    setuptools.setup(**METADATA)
+  except ImportError:
+    import distutils.core
+    distutils.core.setup(**METADATA)
+
+
+if __name__ == '__main__':
+    os.chdir('shout-python')
+    os.system('python setup.py install')
+    os.chdir('..')
+    Main()
