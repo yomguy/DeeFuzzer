72c76d3d87753f735815ddabe37d9add372abde1
yomguy <yomguy@parisson.com>
2012-01-11 15:04:29 +0100

    add pycurl

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ import os
 
 __version__ = '0.6.1'
 __author__ = 'yomguy@parisson.com'
-    
+
 # The base package metadata to be used by both distutils and setuptools
 METADATA = dict(
   name = "DeeFuzzer",
   version = __version__,
   py_modules = ['deefuzzer'],
   description='an easy and instant media streaming tool',
-  author='Guillaume Pellerin', 
+  author='Guillaume Pellerin',
   author_email='yomguy@parisson.com',
   license='CeCILL',
   url='http://svn.parisson.org/deefuzzer',
   keywords='audio video streaming broadcast shout',
-  install_requires = ['setuptools', 'tinyurl', 'python-shout', 'python-twitter', 'mutagen', 'pyliblo'],
-  packages=['deefuzzer', 'deefuzzer.tools'], 
+  install_requires = ['setuptools', 'tinyurl', 'python-shout', 'python-twitter', 'mutagen', 'pyliblo', 'pycurl'],
+  packages=['deefuzzer', 'deefuzzer.tools'],
   include_package_data = True,
-  scripts=['scripts/deefuzzer'], 
+  scripts=['scripts/deefuzzer'],
   classifiers = ['Programming Language :: Python', 'Topic :: Internet :: WWW/HTTP :: Dynamic Content', 'Topic :: Multimedia :: Sound/Audio', 'Topic :: Multimedia :: Sound/Audio :: Players',],
   long_description=open('README.rst').read(),
 )
@@ -35,6 +35,6 @@ def Main():
   except ImportError:
     import distutils.core
     distutils.core.setup(**METADATA)
-    
+
 if __name__ == '__main__':
     Main()
