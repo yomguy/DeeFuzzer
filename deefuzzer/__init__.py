a5117fbc19a270e62f60f656aeda31cbbf7943eb
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-01-11 18:27:18 +0100

    bump to 0.8.2

--- deefuzzer/__init__.py
+++ deefuzzer/__init__.py
@@ -2,4 +2,4 @@ from .core import *
 from .station import *
 from .tools import *
 
-__version__ = '0.8.1'
+__version__ = '0.8.2'
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=open('README.rst').read(),
     author="Guillaume Pellerin",
     author_email="yomguy@parisson.com",
-    version='0.8.1',
+    version='0.8.2',
     install_requires=[
         'setuptools',
         'wheel',
